0d8d528704de6c7576c2aec354361557d19d9779
virtu <virtu@cryptic.to>
2024-05-06 15:22:40 +0200

    Dump entire getnodeaddresses output

--- src/bitcoin_monitor/bitcoin/rpc/getnodeaddresses.py
+++ src/bitcoin_monitor/bitcoin/rpc/getnodeaddresses.py
@@ -1,6 +1,10 @@
 """Bitcoin Core RPC API implementation."""
 
+import csv
+import logging as log
+import lzma
 from dataclasses import dataclass
+from pathlib import Path
 from typing import ClassVar
 
 from . import BitcoinRPCBase
@@ -12,19 +16,36 @@ class GetNodeAddresses(BitcoinRPCBase):
 
     CALL_NAME: ClassVar[str] = "getnodeaddresses"
     CALL_ARGUMENTS: ClassVar[list] = [0]
-    CSV_FIELDS: ClassVar[list[str]] = ["ipv4", "ipv6", "onion", "i2p", "cjdns"]
-    FREQUENCY: ClassVar[int] = 60 * 10  # every ten minutes
+    CSV_FIELDS: ClassVar[list[str]] = ["time", "services", "address", "port", "network"]
+    FREQUENCY: ClassVar[int] = 60 * 60  # every hour
 
     def format_results(self, timestamp, data) -> list[dict]:
         """
         Format RPC call result.
 
-        The RPC call result is a scalar integer: Nothing to do.
+        The RPC call result is a list of detailed address data entries.
         """
 
-        result = {"timestamp": timestamp}
-        for net in GetNodeAddresses.CSV_FIELDS:
-            net_addrs = [1 for addr in data if addr["network"] == net]
-            result[net] = len(net_addrs)
+        results = []
+        for addr in data:
+            result = {"timestamp": timestamp}
+            result.update({key: addr[key] for key in self.CSV_FIELDS if key in addr})
+            results.append(result)
 
-        return [result]
+        return results
+
+    def write_result(self, data):
+        """Custom write result method."""
+
+        timestamp_str = data[0]["timestamp"]
+        file = Path(f"{self.results_path}/{self.CALL_NAME}/{timestamp_str}.csv.xz")
+
+        if file.exists():
+            log.error("output file %s already exists. Skipping.", file)
+            return
+
+        file.parent.mkdir(parents=True, exist_ok=True)
+        with lzma.open(file, "wt") as f:
+            csv_writer = csv.DictWriter(f, fieldnames=["timestamp"] + self.CSV_FIELDS)
+            csv_writer.writeheader()
+            csv_writer.writerows(data)
