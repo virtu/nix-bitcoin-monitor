7b887d027899058d59df43988dc65a11cec9108f
virtu <virtu@cryptic.to>
2024-05-23 19:57:43 +0200

    Add support to dump addrman cache

--- src/bitcoin_monitor/bitcoin/rpc/__init__.py
+++ src/bitcoin_monitor/bitcoin/rpc/__init__.py
@@ -2,4 +2,5 @@ from .base import BitcoinRPCBase
 from .getconnectioncount import GetConnectionCount
 from .getnodeaddresses import GetNodeAddresses
 from .getpeerinfo import GetPeerInfo
+from .getrawaddrman import GetRawAddrman
 from .gettxoutsetinfo import GetTxoutSetInfo
