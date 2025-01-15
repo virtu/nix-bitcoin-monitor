8de7ba98a97d6773c74be7e52cdc45877c6136ff
virtu <virtu@21.ninja>
2025-01-15 10:04:27 +0100

    Enable sources according to configuration

--- src/bitcoin_monitor/master.py
+++ src/bitcoin_monitor/master.py
@@ -20,33 +20,44 @@ class Master:
         """Custom logger."""
         return log.getLogger(self.__class__.__name__)
 
+    async def prepare_sources(self) -> list:
+        """Prepare data sources per configuration set via command-line arguments."""
+        sources = []
+
+        # RPC sources
+        args = (self.conf.rpc_conf, self.conf.results_path)
+        if self.conf.sources.rpc_getconnectioncount:
+            sources.append(rpc.GetConnectionCount(*args))
+        if self.conf.sources.rpc_getpeerinfo:
+            sources.append(rpc.GetPeerInfo(*args))
+        if self.conf.sources.rpc_gettxoutsetinfo:
+            sources.append(rpc.GetTxoutSetInfo(*args))
+        if self.conf.sources.rpc_getnodeaddresses:
+            sources.append(rpc.GetNodeAddresses(*args))
+        if self.conf.sources.rpc_getrawaddrman:
+            sources.append(rpc.GetRawAddrman(*args))
+
+        # tracepoint sources
+        args = (self.conf.results_path,)
+        if self.conf.sources.tracepoints_net:
+            sources.append(tracepoints.Net(*args))
+
+        # systemd sources
+        args = (self.conf.results_path,)
+        if self.conf.sources.systemd_ipaccounting:
+            sources.append(systemd.IPAccounting(*args))
+
+        return sources
+
     async def run(self):
         """Entry point for the master/control thread."""
 
         while True:
             self.log.info("thread started")
 
-            args = (self.conf.rpc_conf, self.conf.results_path)
-            get_connection_count = rpc.GetConnectionCount(*args)
-            get_peer_info = rpc.GetPeerInfo(*args)
-            get_txoutset_info = rpc.GetTxoutSetInfo(*args)
-            get_node_addresses = rpc.GetNodeAddresses(*args)
-            get_raw_addrman = rpc.GetRawAddrman(*args)
-
-            args = (self.conf.results_path,)
-            ip_accounting = systemd.IPAccounting(*args)
-
-            args = (self.conf.results_path,)
-            net = tracepoints.Net(*args)
-
+            sources = await self.prepare_sources()
             await asyncio.gather(
-                get_connection_count.run(),
-                get_peer_info.run(),
-                get_txoutset_info.run(),
-                get_node_addresses.run(),
-                get_raw_addrman.run(),
-                ip_accounting.run(),
-                net.run(),
+                *[sensor.run() for sensor in sources],
             )
             self.log.info("sleeping for five")
             await asyncio.sleep(5)
